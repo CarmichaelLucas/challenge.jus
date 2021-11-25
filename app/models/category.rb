e04f0b797c38545b09578852c5b8b925e0f709e0
Lucas Carmichael <carmichael.lucas@outlook.com>
2021-11-24 23:12:56 -0300

    passando lint no projeto

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,10 +6,8 @@ class Category < ApplicationRecord
   def self.find_or_create_default
     category = Category.find_or_initialize_by(name: 'default')
 
-    if category.new_record?
-      category.save! 
-    end
-    
+    category.save! if category.new_record?
+
     category
   end
 end
